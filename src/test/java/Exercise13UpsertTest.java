592abe40e9c0a2f0811a45f9ab1354c6ca4b0d1b
dvelopp <dvelopp@gmail.com>
2017-01-04 22:53:42 +0200

    Exercise 13-14 cleanup

--- src/test/java/Exercise13UpsertTest.java
+++ src/test/java/Exercise13UpsertTest.java
@@ -7,9 +7,9 @@ import person.Person;
 import person.PersonAdaptor;
 
 import java.net.UnknownHostException;
-import java.util.Collections;
 
 import static java.util.Arrays.asList;
+import static java.util.Collections.emptyList;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
@@ -22,38 +22,34 @@ public class Exercise13UpsertTest {
     @Test
     public void shouldOnlyInsertDBObjectIfItDidNotExistWhenUpsertIsTrue() {
         // Given
-        Person bob = new Person("bob", "Bob The Amazing", new Address("123 Fake St", "LondonTown", 1234567890), asList(27464, 747854));
+        Person bob = new Person("bob", "Bob The Amazing", new Address("123 Fake St", "LondonTown", 1234567890),
+                asList(27464, 747854));
         collection.insert(PersonAdaptor.toDBObject(bob));
-
-        Person charlie = new Person("charlie", "Charles", new Address("74 That Place", "LondonTown", 1234567890), asList(1, 74));
+        Person charlie = new Person("charlie", "Charles", new Address("74 That Place", "LondonTown", 1234567890),
+                asList(1, 74));
         collection.insert(PersonAdaptor.toDBObject(charlie));
-
         // new person not in the database yet
-        Person claire = new Person("claire", "Claire", new Address("1", "Town", 836558493), Collections.<Integer>emptyList());
-
+        Person claire = new Person("claire", "Claire", new Address("1", "Town", 836558493), emptyList());
         // When
         DBObject findClaire = new BasicDBObject("_id", claire.getId());
-        WriteResult resultOfUpdate = collection.update(findClaire, new BasicDBObject("$set", new BasicDBObject("wasUpdated", true)));
-
+        WriteResult resultOfUpdate = collection.update(findClaire, new BasicDBObject("$set",
+                new BasicDBObject("wasUpdated", true)));
         // Then
         assertThat(resultOfUpdate.getN(), is(0));
         // without upsert this should not have been inserted
         assertThat(collection.find(findClaire).count(), is(0));
-
-
         // When
-        WriteResult resultOfUpsert = collection.update(findClaire, new BasicDBObject("$set", new BasicDBObject("wasUpdated", true)), true, false);
-
+        WriteResult resultOfUpsert = collection.update(findClaire, new BasicDBObject("$set",
+                new BasicDBObject("wasUpdated", true)), true, false);
         // Then
         assertThat(resultOfUpsert.getN(), is(1));
-
         DBObject newClaireDBObject = collection.find(findClaire).toArray().get(0);
         // all values should have been updated to the new object values
-        assertThat((String) newClaireDBObject.get("_id"), is(claire.getId()));
+        assertThat(newClaireDBObject.get("_id"), is(claire.getId()));
     }
 
     @Before
-    public void setUp() throws UnknownHostException, UnknownHostException {
+    public void setUp() throws UnknownHostException {
         MongoClient mongoClient = new MongoClient(new MongoClientURI("mongodb://localhost:27017"));
         database = mongoClient.getDB("Examples");
         collection = database.getCollection("people");
--- src/test/java/Exercise14RemoveTest.java
+++ src/test/java/Exercise14RemoveTest.java
@@ -7,10 +7,10 @@ import person.Person;
 import person.PersonAdaptor;
 
 import java.net.UnknownHostException;
-import java.util.Collections;
 import java.util.List;
 
 import static java.util.Arrays.asList;
+import static java.util.Collections.emptyList;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.not;
 import static org.junit.Assert.assertThat;
@@ -23,53 +23,45 @@ public class Exercise14RemoveTest {
     @Test
     public void shouldDeleteOnlyCharlieFromTheDatabase() {
         // Given
-        Person bob = new Person("bob", "Bob The Amazing", new Address("123 Fake St", "LondonTown", 1234567890), asList(27464, 747854));
+        Person bob = new Person("bob", "Bob The Amazing", new Address("123 Fake St", "LondonTown", 1234567890),
+                asList(27464, 747854));
         collection.insert(PersonAdaptor.toDBObject(bob));
-
-        Person charlie = new Person("charlie", "Charles", new Address("74 That Place", "LondonTown", 1234567890), asList(1, 74));
+        Person charlie = new Person("charlie", "Charles", new Address("74 That Place", "LondonTown", 1234567890),
+                asList(1, 74));
         collection.insert(PersonAdaptor.toDBObject(charlie));
-
-        Person emily = new Person("emily", "Emily", new Address("5", "Some Town", 646383), Collections.<Integer>emptyList());
+        Person emily = new Person("emily", "Emily", new Address("5", "Some Town", 646383), emptyList());
         collection.insert(PersonAdaptor.toDBObject(emily));
-
         // When
         DBObject query = new BasicDBObject("_id", charlie.getId());
         WriteResult resultOfRemove = collection.remove(query);
-
         // Then
         assertThat(resultOfRemove.getN(), is(1));
-
         List<DBObject> remainingPeople = collection.find().toArray();
         assertThat(remainingPeople.size(), is(2));
-
         for (final DBObject remainingPerson : remainingPeople) {
-            assertThat((String) remainingPerson.get("_id"), is(not(charlie.getId())));
+            assertThat(remainingPerson.get("_id"), is(not(charlie.getId())));
         }
     }
 
     @Test
     public void shouldDeletePeopleWhoLiveInLondon() {
         // Given
-        Person bob = new Person("bob", "Bob The Amazing", new Address("123 Fake St", "LondonTown", 1234567890), asList(27464, 747854));
+        Person bob = new Person("bob", "Bob The Amazing", new Address("123 Fake St", "LondonTown", 1234567890),
+                asList(27464, 747854));
         collection.insert(PersonAdaptor.toDBObject(bob));
-
-        Person charlie = new Person("charlie", "Charles", new Address("74 That Place", "LondonTown", 1234567890), asList(1, 74));
+        Person charlie = new Person("charlie", "Charles", new Address("74 That Place", "LondonTown", 1234567890),
+                asList(1, 74));
         collection.insert(PersonAdaptor.toDBObject(charlie));
-
-        Person emily = new Person("emily", "Emily", new Address("5", "Some Town", 646383), Collections.<Integer>emptyList());
+        Person emily = new Person("emily", "Emily", new Address("5", "Some Town", 646383), emptyList());
         collection.insert(PersonAdaptor.toDBObject(emily));
-
         // When
         DBObject query = new BasicDBObject("address.city", "LondonTown");
         WriteResult resultOfRemove = collection.remove(query);
-
         // Then
         assertThat(resultOfRemove.getN(), is(2));
-
         List<DBObject> remainingPeople = collection.find().toArray();
         assertThat(remainingPeople.size(), is(1));
-
-        assertThat((String) remainingPeople.get(0).get("_id"), is(emily.getId()));
+        assertThat(remainingPeople.get(0).get("_id"), is(emily.getId()));
     }
 
     @Before
