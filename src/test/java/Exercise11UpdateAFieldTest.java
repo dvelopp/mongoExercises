3bca40f66eb2be2d0a8882fbc002c719920d8dfa
dvelopp <dvelopp@gmail.com>
2017-01-04 22:51:25 +0200

    Exercise 11-12 cleanup

--- src/test/java/Exercise11UpdateAFieldTest.java
+++ src/test/java/Exercise11UpdateAFieldTest.java
@@ -16,80 +16,76 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
 public class Exercise11UpdateAFieldTest {
+
     private DB database;
     private DBCollection collection;
 
     @Test
     public void shouldUpdateCharliesAddress() {
         // Given
-        Person bob = new Person("bob", "Bob The Amazing", new Address("123 Fake St", "LondonTown", 1234567890), asList(27464, 747854));
+        Person bob = new Person("bob", "Bob The Amazing", new Address("123 Fake St", "LondonTown", 1234567890),
+                asList(27464, 747854));
         collection.insert(PersonAdaptor.toDBObject(bob));
-
-        Person charlie = new Person("charlie", "Charles", new Address("74 That Place", "LondonTown", 1234567890), asList(1, 74));
+        Person charlie = new Person("charlie", "Charles", new Address("74 That Place", "LondonTown", 1234567890),
+                asList(1, 74));
         collection.insert(PersonAdaptor.toDBObject(charlie));
-
         String charliesNewAddress = "987 The New Street";
-
         // When
         DBObject findCharlie = new BasicDBObject("_id", charlie.getId());
-        WriteResult resultOfUpdate = collection.update(findCharlie, new BasicDBObject("$set", new BasicDBObject("address.street", charliesNewAddress)));
-
+        WriteResult resultOfUpdate = collection.update(findCharlie, new BasicDBObject("$set",
+                new BasicDBObject("address.street", charliesNewAddress)));
         // Then
         assertThat(resultOfUpdate.getN(), is(1));
 
         DBObject newCharlie = collection.find(findCharlie).toArray().get(0);
         // this stuff should all be the same
-        assertThat((String) newCharlie.get("_id"), is(charlie.getId()));
-        assertThat((String) newCharlie.get("name"), is(charlie.getName()));
-
+        assertThat(newCharlie.get("_id"), is(charlie.getId()));
+        assertThat(newCharlie.get("name"), is(charlie.getName()));
         // the address street, and only the street, should have changed
         DBObject address = (DBObject) newCharlie.get("address");
-        assertThat((String) address.get("street"), is(charliesNewAddress));
-        assertThat((String) address.get("city"), is(charlie.getAddress().getTown()));
-        assertThat((int) address.get("phone"), is(charlie.getAddress().getPhone()));
+        assertThat(address.get("street"), is(charliesNewAddress));
+        assertThat(address.get("city"), is(charlie.getAddress().getTown()));
+        assertThat(address.get("phone"), is(charlie.getAddress().getPhone()));
     }
 
     @Test
     public void shouldAddANewFieldToAnExistingDocument() {
         // Given
-        Person bob = new Person("bob", "Bob The Amazing", new Address("123 Fake St", "LondonTown", 1234567890), asList(27464, 747854));
+        Person bob = new Person("bob", "Bob The Amazing", new Address("123 Fake St", "LondonTown", 1234567890),
+                asList(27464, 747854));
         collection.insert(PersonAdaptor.toDBObject(bob));
-
-        Person charlie = new Person("charlie", "Charles", new Address("74 That Place", "LondonTown", 1234567890), asList(1, 74));
+        Person charlie = new Person("charlie", "Charles", new Address("74 That Place", "LondonTown", 1234567890),
+                asList(1, 74));
         collection.insert(PersonAdaptor.toDBObject(charlie));
-
         // When
         DBObject findCharlie = new BasicDBObject("_id", charlie.getId());
-        WriteResult resultOfUpdate = collection.update(findCharlie, new BasicDBObject("$set", new BasicDBObject("newField", "A New Value")));
-
+        WriteResult resultOfUpdate = collection.update(findCharlie, new BasicDBObject("$set",
+                new BasicDBObject("newField", "A New Value")));
         // Then
         assertThat(resultOfUpdate.getN(), is(1));
-
         DBObject newCharlie = collection.find(findCharlie).toArray().get(0);
         // this stuff should all be the same
-        assertThat((String) newCharlie.get("_id"), is(charlie.getId()));
-        assertThat((String) newCharlie.get("name"), is(charlie.getName()));
-        assertThat((String) newCharlie.get("newField"), is("A New Value"));
+        assertThat(newCharlie.get("_id"), is(charlie.getId()));
+        assertThat(newCharlie.get("name"), is(charlie.getName()));
+        assertThat(newCharlie.get("newField"), is("A New Value"));
     }
 
     //BONUS
     @Test
     public void shouldAddAnotherBookToBobsBookIds() {
         // Given
-        Person bob = new Person("bob", "Bob The Amazing", new Address("123 Fake St", "LondonTown", 1234567890), asList(27464, 747854));
+        Person bob = new Person("bob", "Bob The Amazing", new Address("123 Fake St", "LondonTown", 1234567890),
+                asList(27464, 747854));
         collection.insert(PersonAdaptor.toDBObject(bob));
-
-        Person charlie = new Person("charlie", "Charles", new Address("74 That Place", "LondonTown", 1234567890), asList(1, 74));
+        Person charlie = new Person("charlie", "Charles", new Address("74 That Place", "LondonTown", 1234567890),
+                asList(1, 74));
         collection.insert(PersonAdaptor.toDBObject(charlie));
-
         // When
         DBObject findBob = new BasicDBObject("_id", bob.getId());
         collection.update(findBob, new BasicDBObject("$push", new BasicDBObject("books", 66)));
         // Then
         DBObject newBob = collection.find(findBob).toArray().get(0);
-
-        assertThat((String) newBob.get("name"), is(bob.getName()));
-
+        assertThat(newBob.get("name"), is(bob.getName()));
         // there should be another item in the array
         List<Integer> bobsBooks = (List<Integer>) newBob.get("books");
         // note these are  ordered
--- src/test/java/Exercise12UpdateMultipleDocumentsTest.java
+++ src/test/java/Exercise12UpdateMultipleDocumentsTest.java
@@ -7,10 +7,10 @@ import person.Person;
 import person.PersonAdaptor;
 
 import java.net.UnknownHostException;
-import java.util.Collections;
 import java.util.List;
 
 import static java.util.Arrays.asList;
+import static java.util.Collections.emptyList;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.core.IsNull.nullValue;
 import static org.junit.Assert.assertThat;
@@ -24,29 +24,24 @@ public class Exercise12UpdateMultipleDocumentsTest {
     @Test
     public void shouldOnlyUpdateTheFirstDBObjectMatchingTheQuery() {
         // Given
-        Person bob = new Person("bob", "Bob The Amazing", new Address("123 Fake St", "LondonTown", 1234567890), asList(27464, 747854));
+        Person bob = new Person("bob", "Bob The Amazing", new Address("123 Fake St", "LondonTown", 1234567890),
+                asList(27464, 747854));
         collection.insert(PersonAdaptor.toDBObject(bob));
-
-        Person charlie = new Person("charlie", "Charles", new Address("74 That Place", "LondonTown", 1234567890), asList(1, 74));
+        Person charlie = new Person("charlie", "Charles", new Address("74 That Place", "LondonTown", 1234567890),
+                asList(1, 74));
         collection.insert(PersonAdaptor.toDBObject(charlie));
-
-        Person emily = new Person("emily", "Emily", new Address("5", "Some Town", 646383), Collections.<Integer>emptyList());
+        Person emily = new Person("emily", "Emily", new Address("5", "Some Town", 646383), emptyList());
         collection.insert(PersonAdaptor.toDBObject(emily));
-
         // When
         DBObject findLondoners = new BasicDBObject("address.city", "LondonTown");
         assertThat(collection.find(findLondoners).count(), is(2));
-
         collection.update(findLondoners, new BasicDBObject("$set", new BasicDBObject("wasUpdated", true)));
-
         // Then
         List<DBObject> londoners = collection.find(findLondoners).sort(new BasicDBObject("_id", 1)).toArray();
         assertThat(londoners.size(), is(2));
-
-        assertThat((String) londoners.get(0).get("name"), is(bob.getName()));
-        assertThat((boolean) londoners.get(0).get("wasUpdated"), is(true));
-
-        assertThat((String) londoners.get(1).get("name"), is(charlie.getName()));
+        assertThat(londoners.get(0).get("name"), is(bob.getName()));
+        assertThat(londoners.get(0).get("wasUpdated"), is(true));
+        assertThat(londoners.get(1).get("name"), is(charlie.getName()));
         assertThat(londoners.get(1).get("wasUpdated"), is(nullValue()));
     }
 
@@ -54,32 +49,27 @@ public class Exercise12UpdateMultipleDocumentsTest {
     @Test
     public void shouldUpdateEveryoneLivingInLondon() {
         // Given
-        Person bob = new Person("bob", "Bob The Amazing", new Address("123 Fake St", "LondonTown", 1234567890), asList(27464, 747854));
+        Person bob = new Person("bob", "Bob The Amazing", new Address("123 Fake St", "LondonTown", 1234567890),
+                asList(27464, 747854));
         collection.insert(PersonAdaptor.toDBObject(bob));
-
-        Person charlie = new Person("charlie", "Charles", new Address("74 That Place", "LondonTown", 1234567890), asList(1, 74));
+        Person charlie = new Person("charlie", "Charles", new Address("74 That Place", "LondonTown", 1234567890),
+                asList(1, 74));
         collection.insert(PersonAdaptor.toDBObject(charlie));
-
-        Person emily = new Person("emily", "Emily", new Address("5", "Some Town", 646383), Collections.<Integer>emptyList());
+        Person emily = new Person("emily", "Emily", new Address("5", "Some Town", 646383), emptyList());
         collection.insert(PersonAdaptor.toDBObject(emily));
-
         // When
         DBObject findLondoners = new BasicDBObject("address.city", "LondonTown");
         assertThat(collection.find(findLondoners).count(), is(2));
-
         collection.update(findLondoners, new BasicDBObject("$set", new BasicDBObject("wasUpdated", true)), false, true);
-
         // Then
         List<DBObject> londoners = collection.find(findLondoners).sort(new BasicDBObject("_id", 1)).toArray();
         assertThat(londoners.size(), is(2));
-
         DBObject firstLondoner = londoners.get(0);
-        assertThat((String) firstLondoner.get("name"), is(bob.getName()));
-        assertThat((boolean) firstLondoner.get("wasUpdated"), is(true));
-
+        assertThat(firstLondoner.get("name"), is(bob.getName()));
+        assertThat(firstLondoner.get("wasUpdated"), is(true));
         DBObject secondLondoner = londoners.get(1);
-        assertThat((String) secondLondoner.get("name"), is(charlie.getName()));
-        assertThat((boolean) secondLondoner.get("wasUpdated"), is(true));
+        assertThat(secondLondoner.get("name"), is(charlie.getName()));
+        assertThat(secondLondoner.get("wasUpdated"), is(true));
     }
 
     @Before
